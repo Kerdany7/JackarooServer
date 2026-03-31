48313fc3ccb71a8b21656e341d3b1fe6d603dbc6
KerdanyPC <yahiakerdanyy@gmail.com>
2026-03-31 15:25:02 +0200

    Setup

---
+++ src/main/java/com/jackaroo/jackaroo_backend/JackarooBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jackaroo.jackaroo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackarooBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackarooBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackaroo/jackaroo_backend/JackarooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackaroo.jackaroo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackarooBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
